9016628c22f9b736196ad85a0f9199c530c243ea
kfigon <kamil.figon@gmail.com>
2024-07-08 20:04:57 +0200

    print rat

--- sicp/p2/p2.rkt
+++ sicp/p2/p2.rkt
@@ -4,32 +4,41 @@
 
 (define (make-rational a b) (cons a b)) 
 (define (num rat) (car rat))
-(define (denum rat) (cdr rat))
+(define (denom rat) (cdr rat))
 
 ; more performant and clever alternatives
 ; (define make-rational cons)
 ; (define num car)
-; (define denum cdr)
+; (define denom cdr)
 
 ; ax/ay + bx/by = ((axby) + (bxay))/(ay*by)
 (define (add-rat a b) 
     (make-rational 
-        (+ (* (num a) (denum b))
-           (* (num b) (denum a)))
-        (* (denum a) (denum b))))
+        (+ (* (num a) (denom b))
+           (* (num b) (denom a)))
+        (* (denom a) (denom b))))
 
 (define (sub-rat a b) 
     (make-rational 
-        (- (* (num a) (denum b))
-           (* (num b) (denum a)))
-        (* (denum a) (denum b))))
+        (- (* (num a) (denom b))
+           (* (num b) (denom a)))
+        (* (denom a) (denom b))))
 
 (define (mult-rat a b) 
     (make-rational 
                     (* (num a) (num b))
-                    (* (denum a) (denum b))))
+                    (* (denom a) (denom b))))
 
 (define (eq-rat a b) 
-    (= (* (num a) (denum b)) 
-       (* (num b) (denum a))))
+    (= (* (num a) (denom b)) 
+       (* (num b) (denom a))))
 
+
+(define (print-rat x)
+    (newline)
+    (display (num x))
+    (display "/")
+    (display (denom x)))
+
+(print-rat (add-rat (make-rational 1 2) (make-rational 3 4)))
+(print-rat (add-rat (make-rational 1 3) (make-rational 1 3)))
