2fb0a7ec32889fbeb277f38ac702a3e589cf54ab
kfigon <kamil.figon@gmail.com>
2024-07-17 12:58:48 +0200

    ex 2.35 todo

--- sicp/excercises/ex2_combinators.rkt
+++ sicp/excercises/ex2_combinators.rkt
@@ -20,3 +20,23 @@
 (append (list 1) (list 4 5 6))
 
 (length (list 1 2 3 4 5 6))
+
+; 2.35
+; tree len as reduce
+(newline)
+
+(define (tree-len t)
+    (cond 
+          ((null? t) 0)
+          ((not (pair? t)) 1)
+          (else (+ (tree-len (car t)) (tree-len (cdr t))))))
+
+(define (count-leaves t)
+  (reduce 
+        <??>
+        <??>
+        (map <??> <??>)))
+
+(define a-tree (cons (list 1 2) (list 3 4)))
+(tree-len a-tree)
+(count-leaves a-tree)
