b2aa22487a925b48a3a72e0c195f1b2e5340ab0a
kfigon <kamil.figon@gmail.com>
2024-07-17 12:14:55 +0200

    more combinators'

--- sicp/p2/p2_3_combinators.rkt
+++ sicp/p2/p2_3_combinators.rkt
@@ -1,16 +1,41 @@
 #lang racket
 
-(define (map iter fn)
+(define (map fn iter)
     (if (null? iter) null
-        (cons (fn (car iter)) (map (cdr iter) fn))))
+        (cons (fn (car iter)) (map fn (cdr iter)))))
 
-(define (filter iter fn)
+(define (filter fn iter)
     (if (null? iter) null
         (if (fn (car iter)) 
-            (cons (car iter) (filter (cdr iter) fn))
-            (filter (cdr iter) fn))))
+            (cons (car iter) (filter fn (cdr iter)))
+            (filter fn (cdr iter)))))
 
-(map (list 1 2 3 4 5 6) (lambda (x) (* x 3)))
+(define (reduce init fn iter)
+    (if (null? iter) 
+        init
+        (fn (car iter) 
+            (reduce init fn (cdr iter)))))
 
-(define (even v) (= (modulo v 2) 0))
-(filter (list 1 2 3 4 5 6) even)
+(define (enumerate-num low high)
+    (if (> low high) null
+        (cons low (enumerate-num (+ low 1) high))))
+
+(map (lambda (x) (* x 3)) (list 1 2 3 4 5 6))
+
+(define (even? v) (= (modulo v 2) 0))
+(define (odd? v) (not (even? v)))
+
+(filter even? (list 1 2 3 4 5 6))
+(reduce 0 + (list 1 2 3 4 5))
+(reduce 1 * (list 1 2 3 4 5))
+(reduce null cons (list 1 2 3 4 5))
+
+(newline)
+(define (square v) (* v v))
+(define (sum-odd-squares top)
+    (reduce 0 + 
+        (map square 
+            (filter odd? 
+                (enumerate-num 1 top)))))
+
+(sum-odd-squares 6)
