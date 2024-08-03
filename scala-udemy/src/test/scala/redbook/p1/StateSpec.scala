013181f312daa1b9df5cce853c4e6baf571357c6
kfigon <kamil.figon@gmail.com>
2024-08-03 11:12:58 +0200

    state

--- scala-udemy/src/test/scala/redbook/p1/StateSpec.scala
+++ scala-udemy/src/test/scala/redbook/p1/StateSpec.scala
@@ -31,28 +31,4 @@ class StateSpec extends AnyFunSpec {
 
       println(res)
     }
-
-    // better abstraction
-    type Rand[+A] = RNG => (A, RNG)
-    val int: Rand[Int] = _.int
-
-    def unit[A](a: A): Rand[A] = rng => (a, rng)
-
-    def map[A, B](s: Rand[A])(f: A => B): Rand[B] = rng => {
-      val (next, newRng) = s(rng)
-      (f(next), newRng)
-    }
-  }
-
-  describe("state") {
-    case class State[S, A](run: S => (A, S)) {
-      def unit(v: A): State[S, A] = State(st => (v, st))
-
-      def map[B](f: A => B): State[S, B] = State(
-        st => {
-          val (nextV: A, nextSt: S) = run(st)
-          (f(nextV), nextSt)
-        })
-    }
-  }
 }
