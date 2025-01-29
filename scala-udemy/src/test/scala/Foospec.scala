8973fbab4e8485f5969e56b23b080e169da4166a
kfigon <kamil.figon@gmail.com>
2025-01-29 12:25:57 +0100

    reminder of functional features

--- scala-udemy/src/test/scala/Foospec.scala
+++ scala-udemy/src/test/scala/Foospec.scala
@@ -1,11 +1,58 @@
 import org.scalatest.funspec.AnyFunSpec
 
 class Foospec extends AnyFunSpec {
-  implicit class BetterFoo[T](left: T) {
-    def ====(other: T) = if(left != other) throw new Exception(s"$left != $other")
+  it("implicit conversion") {
+    case class Data(v: Int)
+
+    implicit def convertIntToData(v: Int): Data = Data(v+100)
+
+    val v: Data = 1 // int changed to Data with some processing
+    assert(v == Data(101))
   }
 
-  it("foobar") {
+  it("extending existing types") {
+    implicit class BetterInt(left: Int) {
+      def isFoobar(other: Int): Boolean = (left + 1) == other
+    }
+
+    assert(123.isFoobar(124))
+  }
+
+  it("extending existing types - generic") {
+    implicit class BetterFoo[T](left: T) {
+      def ====(other: T) = if(left != other) throw new Exception(s"$left != $other")
+    }
+
     assertThrows[Exception](123 ==== 4567)
+  }
+
+  it("typeclasses - polymorphism with existing types") {
+    trait Serialize[T] {
+      def str(v: T): String
+    }
+
+    implicit object IntSerializer extends Serialize[Int] {
+      override def str(v: Int): String = s"int$v"
+    }
+
+//    there can be multiple implementations for the same type, just import the right one
+//    implicit object AlternativeIntSerializer extends Serialize[Int] {
+//      override def str(v: Int): String = s"foobarInt${v}"
+//    }
+
+    implicit object BoolSerializer extends Serialize[Boolean] {
+      override def str(v: Boolean): String = s"bool$v"
+    }
+
+    // or as a variable
+    implicit val stringSerializer = new Serialize[String] {
+      override def str(v: String): String = s"str_$v"
+    }
+
+    def executor[T](v: T)(implicit serializer: Serialize[T]): String = serializer.str(v)
+
+    assert(executor(123) == "int123")
+    assert(executor(true) == "booltrue")
+    assert(executor("foobar") == "str_foobar")
   }
 }
