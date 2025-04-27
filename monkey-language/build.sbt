bef353ba97f5c4f68c778e67a5e79916a011334a
kfigon <kamil.figon@gmail.com>
2025-04-27 21:38:04 +0200

    hello world monkey lang

---
+++ monkey-language/build.sbt
@@ -0,0 +1,2 @@
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.18"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.18" % "test"
---
+++ monkey-language/src/test/scala/FooSpec.scala
@@ -0,0 +1,9 @@
+import org.scalatest.funspec.AnyFunSpec
+
+class FooSpec extends AnyFunSpec {
+  describe("foobar") {
+    it("asdf") {
+      assert(false)
+    }
+  }
+}
